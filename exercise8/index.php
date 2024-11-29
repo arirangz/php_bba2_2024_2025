00c0a55e595786c852573f71d275ce2ca6664db0
Anthony Laplane <anthony.laplane@gmail.com>
2024-11-29 14:34:20 +0100

    exercise 8

--- exercise8/index.php
+++ exercise8/index.php
@@ -1,4 +1,5 @@
-<?php require_once 'header.php' ?>
+<?php require_once 'header.php';
+?>
 
 <h1>Home</h1>
 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam totam eligendi veritatis nobis voluptates magni magnam a minus, et, neque suscipit. Voluptatibus consectetur harum illo debitis explicabo possimus repellendus et.</p>
